9434ddc6563cfda1ac7e46e1842c160ac1f9f38e
Abir Chodha <abirchodha1@gmail.com>
2025-02-28 22:59:35 +0530

    Create basic routes for all url

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -2,4 +2,20 @@ const getAllTasks = (req, res) => {
   res.send("all items");
 };
 
-module.exports = getAllTasks;
+const createTask = (req, res) => {
+  res.send("Create task");
+};
+
+const getTask = (req, res) => {
+  res.send("Get single task");
+};
+
+const updateTask = (req, res) => {
+  res.send("Update task");
+};
+
+const deleteTask = (req, res) => {
+  res.send("Delete task");
+};
+
+module.exports = { getAllTasks, createTask, getTask, updateTask, deleteTask };
