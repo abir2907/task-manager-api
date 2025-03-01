b6ecc5120ab5578de49729f578d9c7eb6b214012
Abir Chodha <abirchodha1@gmail.com>
2025-03-01 05:35:46 +0530

    Get single task by ID

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -18,8 +18,14 @@ const createTask = async (req, res) => {
   }
 };
 
-const getTask = (req, res) => {
-  res.send("Get single task");
+const getTask = async (req, res) => {
+  try {
+    const { id: taskID } = req.params;
+    const task = await Task.findOne({ _id: taskID });
+    res.status(200).json({ task });
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
 };
 
 const updateTask = (req, res) => {
