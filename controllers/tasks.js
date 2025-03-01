17370a5b011b5d63b058d79f283e88fdb8baa2d2
Abir Chodha <abirchodha1@gmail.com>
2025-03-01 22:11:38 +0530

    Add update task functionality

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -31,8 +31,19 @@ const getTask = async (req, res) => {
   }
 };
 
-const updateTask = (req, res) => {
-  res.send("Update task");
+const updateTask = async (req, res) => {
+  try {
+    const { id: taskID } = req.params;
+    const newData = req.body; // newData has {"name": xyz, "completed": xyz}
+    const options = { new: true, runValidators: true };
+    const task = await Task.findOneAndUpdate({ _id: taskID }, newData, options);
+    if (!task) {
+      return res.status(404).json({ msg: `No task with  id: ${taskID}` });
+    }
+    res.status(200).json({ task });
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
 };
 
 const deleteTask = async (req, res) => {
